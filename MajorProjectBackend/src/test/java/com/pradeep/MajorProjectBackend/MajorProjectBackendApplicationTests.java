caafd89645e6f925a9988bebc2d3960ef883bb4b
pradeep007nc <pradeepnaidu2486@gmail.com>
2023-10-01 15:00:12 +0530

    first boostrap backend code commit

---
+++ MajorProjectBackend/src/test/java/com/pradeep/MajorProjectBackend/MajorProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeep.MajorProjectBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajorProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
